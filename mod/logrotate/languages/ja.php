2a336161dc294f5f56b269aecdaef6d7ac494bad
坂口裕隆 SAKAGUCHI, Hirotaka <kemo@saka-hv.org>
2014-06-15 18:02:14 +0900

    Elgg 1.8.19 language files Japanese translation Initial commit
    	new file:   documentation/examples/plugins/languages/ja.php
    	new file:   install/languages/ja.php
    	new file:   languages/ja.php
    	new file:   mod/blog/languages/ja.php
    	new file:   mod/bookmarks/languages/ja.php
    	new file:   mod/categories/languages/ja.php
    	new file:   mod/custom_index/languages/ja.php
    	new file:   mod/dashboard/languages/ja.php
    	new file:   mod/developers/languages/ja.php
    	new file:   mod/diagnostics/languages/ja.php
    	new file:   mod/embed/languages/ja.php
    	new file:   mod/externalpages/languages/ja.php
    	new file:   mod/file/languages/ja.php
    	new file:   mod/garbagecollector/languages/ja.php
    	new file:   mod/groups/languages/ja.php
    	new file:   mod/invitefriends/languages/ja.php
    	new file:   mod/likes/languages/ja.php
    	new file:   mod/logbrowser/languages/ja.php
    	new file:   mod/logrotate/languages/ja.php
    	new file:   mod/members/languages/ja.php
    	new file:   mod/messageboard/languages/ja.php
    	new file:   mod/messages/languages/ja.php
    	new file:   mod/notifications/languages/ja.php
    	new file:   mod/pages/languages/ja.php
    	new file:   mod/profile/languages/ja.php
    	new file:   mod/reportedcontent/languages/ja.php
    	new file:   mod/search/languages/ja.php
    	new file:   mod/tagcloud/languages/ja.php
    	new file:   mod/thewire/languages/ja.php
    	new file:   mod/tinymce/languages/ja.php
    	new file:   mod/twitter_api/languages/ja.php
    	new file:   mod/uservalidationbyemail/languages/ja.php
    	new file:   views/default/js/languages/ja.php

---
+++ mod/logrotate/languages/ja.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Elgg log rotator language pack.
+ * 
+ * @package ElggLogRotate
+ * @version 1.8.16
+ * @update 2013-7-1
+ */
+
+$japanese = array(
+	'logrotate:period' => 'システムログを保管する頻度',
+	
+	'logrotate:weekly' => '１週間に１回',
+	'logrotate:monthly' => '１ヶ月に１回',
+	'logrotate:yearly' => '１年に１回',
+	
+	'logrotate:logrotated' => 'ログをローテートしました\n',
+	'logrotate:lognotrotated' => "ログローテトに失敗しました\n",
+	'logrotate:delete' => '保存したログの削除',
+
+	'logrotate:week' => 'この1週より古い',
+	'logrotate:month' => 'この１ヶ月よりも古い',
+	'logrotate:year' => 'この１年よりも古い',
+	'logrotate:never' => 'しない',
+		
+	'logrotate:logdeleted' => "ログを削除しました\n",
+	'logrotate:lognotdeleted' => "削除したログはありません\n",
+);
+					
+add_translation("ja",$japanese);
