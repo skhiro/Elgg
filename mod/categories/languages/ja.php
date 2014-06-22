c24020c64eb1acba216717fbd3cd1a7509d52bd2
坂口裕隆 SAKAGUCHI, Hirotaka <kemo@saka-hv.org>
2014-06-22 17:09:55 +0900

    mod/categories/languages/ja.php

--- mod/categories/languages/ja.php
+++ mod/categories/languages/ja.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * Categories 日本語 language file
- *
- */
-$japanese = array(
+return array(
 	'categories' => 'カテゴリ',
 	'categories:settings' => 'サイトカテゴリの設定',
 	'categories:explanation' => 'サイトカテゴリを予め設定しておくことにより、サイトシステム全体でそのカテゴリが利用できるようになります。カテゴリが複数ある場合は、それぞれをカンマ(,)で区切って入力してください。互換性のあるツールであれば、ユーザがコンテンツを新規作成、または編集するときに画面にカテゴリ設定欄が表示されるでしょう。',
@@ -11,6 +7,5 @@ $japanese = array(
 	'categories:results' => "サイトカテゴリの結果: %s",
 	'categories:on_activate_reminder' => "サイト全域に渡るカテゴリは、あなたがカテゴリを追加しない限り機能しません。 <a href=\"%s\">カテゴリを追加</a>してください。",
 	);
-					
-add_translation("ja",$japanese);
+
 
