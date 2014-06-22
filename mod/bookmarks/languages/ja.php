af78e5686346f1c9f672a5097e79f92dd1cc49ff
坂口裕隆 SAKAGUCHI, Hirotaka <kemo@saka-hv.org>
2014-06-22 16:06:38 +0900

    mod/bookmarks/languages/ja.php

--- mod/bookmarks/languages/ja.php
+++ mod/bookmarks/languages/ja.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Bookmarks English language file
- *
- */
-
-/**
  *------------------------------------------------------------------
  * 以下は、このファイルで(Email 通知に使われるメールのサブジェクト)に使われるキー名です。
  * 必要に応じて内容を書き換えて使用すると便利です。
@@ -14,7 +9,7 @@
  *
  */
 
-$japanese = array(
+return array(
 	
 	/**
 	 * Menu items and titles
@@ -31,20 +26,21 @@ $japanese = array(
 	 'bookmarks:bookmarklet' => "ブックマークレットの取得",
 	 'bookmarks:bookmarklet:group' => "グループのブックマークレットの取得",
 	 'bookmarks:inbox' => "Bookmarks inbox",//これを参照しているところはありません。
-	 'bookmarks:morebookmarks' => "もっと見る",
-	 'bookmarks:more' => "もっと見る",
 	 'bookmarks:with' => "共有するメンバーの選択",
 	 'bookmarks:new' => "新しいブックマークが追加されました",
 	 'bookmarks:address' => "ブックマークのアドレス",
 	 'bookmarks:none' => 'ブックマークはひとつも登録されていません',
-	
-	'bookmarks:notification' =>
-'%s さんが新しいブックマークを追加しました。:
 
-%s - %s
+     'bookmarks:notify:summary' => '新着ブックマーク「%s」があります。',
+     'bookmarks:notify:subject' => '新着ブックマーク: %s',
+     'bookmarks:notify:body' =>
+'%s さんが新しいブックマークを追加しました: %s
+
+アドレス: %s
+
 %s
 
-閲覧してコメントするには:
+このブックマークに対して閲覧・コメントするには:
 %s
 ',
 	 'bookmarks:delete:confirm' => "削除してもよろしいですか?",
@@ -65,20 +61,17 @@ $japanese = array(
 	 'bookmarks:group' => 'グループブックマーク',
 	 'bookmarks:enablebookmarks' => 'グループブックマークの利用',
 	 'bookmarks:nogroup' => 'このグループには、まだブックマークが登録されていません。',
-	 'bookmarks:more' => 'もっと',
-
-	 'bookmarks:no_title' => 'No title',
 
 	 /**
 	  * Widget and bookmarklet
 	  */
-	 'bookmarks:widget:description' => "あなたの最近のブクマークを表示します。",
+	 'bookmarks:widget:description' => "あなたが最近つけたブックマークを表示します。",
 	
-	 'bookmarks:bookmarklet:description' => "ブックマークのブックマークレットはWebで見つけたサイトの情報を友達や自分のために素早く保存するためのものです。下のボタンをブラウザのリンクバーにドラッグするだけで利用が開始できます。",
+	 'bookmarks:bookmarklet:description' => "ブックマークレットは特殊なボタンで、ブラウザのリンクバーに保存することができます。Webで見つけたリソースを自分のブックマークに保存したり、オプションとして友だちと共有したりすることもできます。この機能を使用するには、ボタンをあなたのブラウザのリンクバーまでドラッグしてください:",
 
 	 'bookmarks:bookmarklet:descriptionie' => "Internet Explorerをお使いの方はブックマークレットアイコンを右クリックしてから「お気に入りに保存」を選択していただき、その後、リンクバーに登録してください。",
 	 
-	 'bookmarks:bookmarklet:description:conclusion' => "いつでもクリックすることによって、訪れたことのあるページを保存することができるようになります",
+	 'bookmarks:bookmarklet:description:conclusion' => "ブラウザのボタンをクリックすると訪れたページをブックマークすることができあます。",
 	
 	 /**
 	  * Status messages
@@ -94,7 +87,6 @@ $japanese = array(
 	 'bookmarks:save:failed' => "ブックマークに登録できませんでした。タイトル欄とアドレス欄に入力したことを確かめて、もう一度試してみてください。",
 	 'bookmarks:save:invalid' => "このブックマークのアドレスはどこか間違っていますので、保存することはできませんでした。",
 	 'bookmarks:delete:failed' => "ブックマークから削除できませんでした。もう一度試してみてください。",
-	
+     'bookmarks:unknown_bookmark' => 'お探しのブックマークを見つけることができません。',
+
 	);
-					
-add_translation("ja",$japanese);
